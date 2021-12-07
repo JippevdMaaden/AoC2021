5c830bd6eb4cc552bd1711a2a7ee24a456c5026b
jippe@vandermaaden.nl <jippe@vandermaaden.nl>
2021-12-07 21:31:06 +0100

    day 7

---
+++ star14/main.py
@@ -0,0 +1,56 @@
+def parse_input(filename):
+    lines_list = []
+    with open(filename, "r") as f:
+        for readline in f:
+            lines_list = list(map(int, readline.split(",")))
+
+    item_dict = {}
+    for item in lines_list:
+        item_count = item_dict.get(item, 0)
+        item_dict[item] = item_count + 1
+
+    return item_dict
+
+
+def count_pyramid_steps(number):
+    # TODO: This takes quite a long time for bigger numbers, find a better way
+    steps = 0
+    for i in range(0, number + 1):
+        steps += i
+
+    return steps
+
+
+def align_crabs(crab_dict, min_value, max_value):
+    fuel_cost_dict = {}
+    for i in range(min_value, max_value + 1):
+        total_fuel_cost = 0
+        for crab_distance, crab_count in crab_dict.items():
+            travel_distance = count_pyramid_steps(abs(crab_distance - i))
+            fuel_cost = travel_distance * crab_count
+            total_fuel_cost += fuel_cost
+
+        fuel_cost_dict[i] = total_fuel_cost
+
+    return fuel_cost_dict
+
+
+def main():
+    crab_dict = parse_input("input.txt")
+    max_value = max(crab_dict.keys())
+    min_value = min(crab_dict.keys())
+
+    fuel_cost_dict = align_crabs(crab_dict, min_value, max_value)
+    print(fuel_cost_dict)
+    lowest_fuel_cost = None
+    for level, fuel_cost in fuel_cost_dict.items():
+        if lowest_fuel_cost:
+            if lowest_fuel_cost > fuel_cost:
+                lowest_fuel_cost = fuel_cost
+        else:
+            lowest_fuel_cost = fuel_cost
+
+    print(lowest_fuel_cost)
+
+
+main()
